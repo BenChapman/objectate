4bb8a8f7d5487483041b5648ee1d54e70e2d9e9d
Ben Chapman <ben@dismiz.com>
2013-01-17 01:12:07 +0000

    Added line of the day to page

--- public/index.php
+++ public/index.php
@@ -6,8 +6,17 @@ require_once __DIR__ . '/../loader.php';
 // Load Klein.php
 require_once __DIR__ . '/../libs/klein.php';
 
-respond('GET', '/', function($request, $response) {
+$lines = file(__DIR__ . '/../line_of_the_day.txt');
+$view_data['lotd'] = $lines[array_rand($lines)];
+
+respond('GET', '/', function($request, $response) use ($view_data) {
 	$view_data['contents'] = __DIR__ . '/../templates/pages/home.phtml';
+	$response->render(__DIR__ . '/../templates/main.phtml', $view_data);
+});
+
+respond('GET', '/poem/[:name]', function($request, $response)  use ($view_data) {
+	$view_data['contents'] = __DIR__ . '/../templates/pages/poem.phtml';
+	$view_data['poem'] = new Anontate_Poem(urldecode($request->name));
 	$response->render(__DIR__ . '/../templates/main.phtml', $view_data);
 });
 
