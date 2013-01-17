894a68b37d41a105b1c40fcfdc035e9e9f6be147
Ben Chapman <ben@dismiz.com>
2013-01-17 10:20:52 +0000

    Add poem_index template and route

--- public/index.php
+++ public/index.php
@@ -14,11 +14,19 @@ respond('GET', '/', function($request, $response) use ($view_data) {
 	$response->render(__DIR__ . '/../templates/main.phtml', $view_data);
 });
 
-respond('GET', '/poem/[:name]', function($request, $response)  use ($view_data) {
-	$view_data['contents'] = __DIR__ . '/../templates/pages/poem.phtml';
-	$view_data['poem'] = new Anontate_Poem(urldecode($request->name));
-	$response->render(__DIR__ . '/../templates/main.phtml', $view_data);
+with('/poems',function() use ($view_data) {
+	respond('GET', '/?', function($request, $response)  use ($view_data) {
+		$view_data['contents'] = __DIR__ . '/../templates/pages/poem_index.phtml';
+		$view_data['index'] = new Anontate_Index('poems');
+		$response->render(__DIR__ . '/../templates/main.phtml', $view_data);
+	});
+	respond('GET', '/[:name]', function($request, $response)  use ($view_data) {
+		$view_data['contents'] = __DIR__ . '/../templates/pages/poem.phtml';
+		$view_data['poem'] = new Anontate_Poem(urldecode($request->name));
+		$response->render(__DIR__ . '/../templates/main.phtml', $view_data);
+	});
 });
+
 
 dispatch();
 
